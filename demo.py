5d41536eeadae531239e92d910ed90e86711c0f8
Ayesha <aqaisar3@uwo.ca>
2023-03-09 08:42:36 -0500

    first

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import random
 
 class GameObject(pygame.sprite.Sprite):
     def __init__(self, x, y, img_path):
+        # here
         super().__init__()
         self.image = pygame.image.load(img_path)
         self.rect = self.image.get_rect()
